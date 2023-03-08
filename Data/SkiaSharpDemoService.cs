ef5ff19a13bf31e1e7e1fa1f3e67e711f319eb07
UchiTesing <uchitesting57@gmail.com>
2023-03-08 18:58:57 +0100

    Create Directory if not exists

--- Data/SkiaSharpDemoService.cs
+++ Data/SkiaSharpDemoService.cs
@@ -11,7 +11,7 @@ namespace UT_GraphicsDemo.Data
 
             try
             {
-                SKImageInfo _imageInfo = new SKImageInfo(320,240);
+                SKImageInfo _imageInfo = new SKImageInfo(320, 240);
                 _surface = SKSurface.Create(_imageInfo);
             }
             catch (Exception)
@@ -21,20 +21,37 @@ namespace UT_GraphicsDemo.Data
             }
         }
 
-        private void RenderImage() {
+        private void RenderImage()
+        {
             using (SKImage image = _surface.Snapshot())
             using (SKData data = image.Encode(SKEncodedImageFormat.Png, 100))
             using (MemoryStream mStream = new MemoryStream(data.ToArray()))
             {
                 var separator = Path.DirectorySeparatorChar;
-                FileStream fs = File.Create("wwwroot"+separator
-                    +"Images"+separator
-                    +"Output"+separator
-                    +"Test.png");
-                var truc = new System.IO.BinaryWriter(fs);
-                truc.Write(mStream.ToArray());
-                truc.Close();
-                fs.Close();
+                var path = "wwwroot" + separator
+                    + "Images" + separator
+                    + "Output" + separator;
+
+                if (!Path.Exists(path))
+                    System.IO.Directory.CreateDirectory(path);
+
+                try
+                {
+                    FileStream fs = File.Create(path + "Test.png");
+                    var truc = new System.IO.BinaryWriter(fs);
+                    truc.Write(mStream.ToArray());
+                    truc.Close();
+                    fs.Close();
+                }
+                catch (System.IO.DirectoryNotFoundException dnfe)
+                {
+                    System.Console.Error.WriteLine("Directory " + path + " was not found.");
+                    System.Console.Error.WriteLine(dnfe.Message);
+                }
+                catch (System.Exception e)
+                {
+                    System.Console.Error.WriteLine("An exception occurred: \n\t" + e.Message);
+                }
             }
         }
 
@@ -54,16 +71,16 @@ namespace UT_GraphicsDemo.Data
                         paint.Color = SKColors.Coral;
                         paint.StrokeWidth = 3;
                         paint.Style = SKPaintStyle.Stroke;
-                        paint.PathEffect = SKPathEffect.CreateDash(new float[] {5.0f,3.0f},0f);
-                        canvas.DrawCircle(150,150,30,paint);
+                        paint.PathEffect = SKPathEffect.CreateDash(new float[] { 5.0f, 3.0f }, 0f);
+                        canvas.DrawCircle(150, 150, 30, paint);
                         paint.Color = SKColors.Chartreuse;
                         canvas.DrawRoundRect(
                             new SKRoundRect(
-                                new SKRect(150,30,210,100),
+                                new SKRect(150, 30, 210, 100),
                                 8),
                             paint);
                         paint.Color = SKColors.OrangeRed;
-                        canvas.DrawRect(new SKRect(5,5,100,100),paint);
+                        canvas.DrawRect(new SKRect(5, 5, 100, 100), paint);
                     }
 
                     RenderImage();
